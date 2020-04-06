5a40f0b97d4c63a8c3350d7d4896ba898e7f3098
fierytortilla <dschlessinger@live.com>
2020-04-06 11:11:29 +0100

    finally fixed CSS

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
 <div id='app'>
-  <header>
+    <header>
     <h1>Ghibli Otaku Fandom Extravaganza</h1>
   </header>
   <div id="container">
     <div id= "ghibli-movies">
     <p>List of Ghibli Movies: <films-list :films="films"/></p>
     <p>Favorited Ghibli Movies: <favorite-films-list /></p>
+    <film-detail /> 
   </div>
 
   <div id="ghibli-characters">
     <p>List of Ghibli Characters: <characters-list :characters="characters" /></p>
     <p>Favorited Ghibli Characters: <favorite-character-list /></p>
+    <character-detail />
   </div>
-  <film-detail /> 
-<div class="vl"></div> 
-  <character-detail />
   </div>
-    <img src="./assets/yakul.gif" alt="">
   <footer>
     <p>Miyazaki is the best.</p>
   </footer>
@@ -86,26 +84,31 @@ export default {
     color: yellow;
   }
 
-  .container {
+  #container {
     display: grid;
     grid-template-columns: repeat(10, 100px);
     grid-template-rows: repeat(10, 100px);
-    max-width: 100%;
     text-align: center;
+    background-image: url('./assets/yakul.gif');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 500px 400px;
+    max-width: 100%;
+    height: auto;
   }
 
   #ghibli-movies {
-    /* display: flex;
-    justify-content: flex-start;
-    align-items: flex-start; */
-    grid-area: 4 / 4 / span 7 / span 8;
+    grid-area: 1 / 1 / span 5 / span 5;
+    color:deeppink;
   }
 
   #ghibli-characters {
-    /* display: flex;
-    justify-content: flex-start;
-    align-items: flex-start; */
-    grid-area: 4 / 40 / span 7 / span 8;
+    grid-area: 1 / 5 / span 5 / span 5;
+    color: deeppink;
+  }
+
+  input {
+    width: 35%;
   }
 
   footer {
